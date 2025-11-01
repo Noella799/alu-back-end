c9bb15bd347421d5914628898c393f088a41ec64
root <Noella799>
2025-11-01 10:16:00 +0000

    0. Gather data from an API - implementation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,45 +1,35 @@
 #!/usr/bin/python3
-""" just using some extra modules """
+"""
+Gather data from an API
+"""
 import requests
 import sys
 
-
-def getName():
-    """ getting user name """
-    payload = {'id': sys.argv[1]}
-    dataTwo = requests.get('https://jsonplaceholder.typicode.com/users',
-                           params=payload)
-    JDataTwo = dataTwo.json()
-    # print(JDataTwo[0]['name']
-    return JDataTwo[0]['name']
-
-
-def getTask():
-    """ get task numbers and todos done  """
-    data = requests.get('https://jsonplaceholder.typicode.com/todos')
-    ToDoList = []
-    taskToDo = 0
-    taskDone = 0
-    JData = data.json()
-    DataLength = len(JData)
-    for i in range(0, DataLength):
-        com = int(sys.argv[1])
-        if JData[i]['userId'] == com:
-            taskToDo += 1
-            if JData[i]['completed'] is True:
-                ToDoList.append(JData[i]['title'])
-                taskDone += 1
-    # print(taskToDo)
-    # print(taskDone)
-    # print(ToDoList)
-    print("Employee {} is done with tasks({}/{}):"
-          .format(getName(), taskDone, taskToDo))
-    Lvalue = len(ToDoList)
-    for j in range(0, Lvalue):
-        print("\t {}".format(ToDoList[j]))
-
-""" addding docs everywhre """
+def get_employee_todo_progress(employee_id):
+    """Get employee TODO progress"""
+    # API endpoints
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    
+    # Get user data
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+    employee_name = user_data.get('name')
+    
+    # Get TODOs data
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+    
+    # Calculate progress
+    total_tasks = len(todos_data)
+    done_tasks = sum(1 for task in todos_data if task['completed'])
+    
+    # Display results
+    print(f"Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):")
+    for task in todos_data:
+        if task['completed']:
+            print(f"\t {task['title']}")
 
 if __name__ == "__main__":
-    """ calling """
-    getTask()
+    if len(sys.argv) > 1:
+        get_employee_todo_progress(int(sys.argv[1]))
