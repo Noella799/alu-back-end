cf2e3cfba5074ce4fc71790163a1eac7e0dcbef7
root <Noella799>
2025-11-03 08:06:31 +0000

    updated

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,24 +1,45 @@
 #!/usr/bin/python3
-"""
-Script that uses REST API to return employee TODO list progress.
-For a given employee ID, displays completed tasks information.
-"""
+""" just using some extra modules """
 import requests
 import sys
 
 
+def getName():
+    """ getting user name """
+    payload = {'id': sys.argv[1]}
+    dataTwo = requests.get('https://jsonplaceholder.typicode.com/users',
+                           params=payload)
+    JDataTwo = dataTwo.json()
+    # print(JDataTwo[0]['name']
+    return JDataTwo[0]['name']
+
+
+def getTask():
+    """ get task numbers and todos done  """
+    data = requests.get('https://jsonplaceholder.typicode.com/todos')
+    ToDoList = []
+    taskToDo = 0
+    taskDone = 0
+    JData = data.json()
+    DataLength = len(JData)
+    for i in range(0, DataLength):
+        com = int(sys.argv[1])
+        if JData[i]['userId'] == com:
+            taskToDo += 1
+            if JData[i]['completed'] is True:
+                ToDoList.append(JData[i]['title'])
+                taskDone += 1
+    # print(taskToDo)
+    # print(taskDone)
+    # print(ToDoList)
+    print("Employee {} is done with tasks({}/{}):"
+          .format(getName(), taskDone, taskToDo))
+    Lvalue = len(ToDoList)
+    for j in range(0, Lvalue):
+        print("\t {}".format(ToDoList[j]))
+
+""" addding docs everywhre """
+
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com"
-    
-    user = requests.get("{}/users/{}".format(base_url, user_id)).json()
-    todos = requests.get("{}/todos".format(base_url),
-                         params={"userId": user_id}).json()
-    
-    completed = [task for task in todos if task.get("completed") is True]
-    
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    
-    for task in completed:
-        print("\t {}".format(task.get("title")))
+    """ calling """
+    getTask()
