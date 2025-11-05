474fa9b0f8d3f42624ce89d9794f6476473373f4
root <Noella799>
2025-11-05 09:20:33 +0000

    api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,45 +1,62 @@
-#!/usr/bin/python3
-""" just using some extra modules """
+cript that fetches and displays employee TODO list progress from an API.
+Uses JSONPlaceholder API to retrieve user and todos information.
+"""
 import requests
 import sys
 
 
-def getName():
-    """ getting user name """
-    payload = {'id': sys.argv[1]}
-    dataTwo = requests.get('https://jsonplaceholder.typicode.com/users',
-                           params=payload)
-    JDataTwo = dataTwo.json()
-    # print(JDataTwo[0]['name']
-    return JDataTwo[0]['name']
+def get_employee_todo_progress(employee_id):
+    """
+    Fetches and displays the TODO list progress for a given employee.
+    
+    Args:
+        employee_id: Integer representing the employee ID
+    """
+    base_url = "https://jsonplaceholder.typicode.com"
+    
+    # Fetch user information
+    user_url = f"{base_url}/users/{employee_id}"
+    user_response = requests.get(user_url)
+    
+    if user_response.status_code != 200:
+        print(f"Error: Unable to fetch user data")
+        return
+    
+    user_data = user_response.json()
+    employee_name = user_data.get('name')
+    
+    # Fetch todos for the user
+    todos_url = f"{base_url}/todos?userId={employee_id}"
+    todos_response = requests.get(todos_url)
+    
+    if todos_response.status_code != 200:
+        print(f"Error: Unable to fetch todos data")
+        return
+    
+    todos_data = todos_response.json()
+    
+    # Calculate progress
+    total_tasks = len(todos_data)
+    completed_tasks = [task for task in todos_data if task.get('completed')]
+    number_of_done_tasks = len(completed_tasks)
+    
+    # Display results
+    print(f"Employee {employee_name} is done with tasks"
+          f"({number_of_done_tasks}/{total_tasks}):")
+    
+    # Display completed task titles
+    for task in completed_tasks:
+        print(f"\t {task.get('title')}")
 
-
-def getTask():
-    """ get task numbers and todos done  """
-    data = requests.get('https://jsonplaceholder.typicode.com/todos')
-    ToDoList = []
-    taskToDo = 0
-    taskDone = 0
-    JData = data.json()
-    DataLength = len(JData)
-    for i in range(0, DataLength):
-        com = int(sys.argv[1])
-        if JData[i]['userId'] == com:
-            taskToDo += 1
-            if JData[i]['completed'] is True:
-                ToDoList.append(JData[i]['title'])
-                taskDone += 1
-    # print(taskToDo)
-    # print(taskDone)
-    # print(ToDoList)
-    print("Employee {} is done with tasks({}/{}):"
-          .format(getName(), taskDone, taskToDo))
-    Lvalue = len(ToDoList)
-    for j in range(0, Lvalue):
-        print("\t {}".format(ToDoList[j]))
-
-""" addding docs everywhre """
 
 if __name__ == "__main__":
-    """ calling """
-    getTask()
+    if len(sys.argv) != 2:
+        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+    
+    try:
+        employee_id = int(sys.argv[1])
+        get_employee_todo_progress(employee_id)
+    except ValueError:
+        print("Error: Employee ID must be an integer")
+        sys.exit(1)
