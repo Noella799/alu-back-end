93ed157acac3e7c2d17b7df06e21273c86706d05
root <Noella799>
2025-11-05 10:34:06 +0000

    api

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,40 +1,56 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-Exports an employee's TODO list data to JSON format.
+Module 2-export_to_JSON
+
+Exports all tasks of a given employee ID to a JSON file in the
+following format:
+
+{
+    "<USER_ID>": [
+        {"task": "TASK_TITLE", "completed": COMPLETED_STATUS, "username": "USERNAME"},
+        ...
+    ]
+}
+
+Usage:
+    ./2-export_to_JSON.py <employee_id>
 """
 
 import json
 import requests
 import sys
 
+
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    if len(sys.argv) != 2:
         print("Usage: ./2-export_to_JSON.py <employee_id>")
         sys.exit(1)
 
     user_id = sys.argv[1]
 
-    # Fetch user info
+    # Fetch user information
     user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-    user_response = requests.get(user_url)
-    user = user_response.json()
+    response = requests.get(user_url)
+    user = response.json()
     username = user.get("username")
 
-    # Fetch todos for the user
+    # Fetch all tasks for the user
     todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
-    todos_response = requests.get(todos_url)
-    todos = todos_response.json()
+    response = requests.get(todos_url)
+    todos = response.json()
 
-    # Build JSON data
-    user_tasks = []
+    # Build list of dicts
+    tasks_list = []
     for task in todos:
-        user_tasks.append({
+        task_dict = {
             "task": task.get("title"),
             "completed": task.get("completed"),
             "username": username
-        })
+        }
+        tasks_list.append(task_dict)
 
-    data = {str(user_id): user_tasks}
+    # Wrap in dictionary with user_id as key
+    data = {str(user_id): tasks_list}
 
     # Save to JSON file
     filename = "{}.json".format(user_id)
